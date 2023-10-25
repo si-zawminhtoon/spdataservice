b4f95dd20ca3af4cd60a989f37d364a85c852475
Zaw Min Htoon <mdcr.zawminhtoon@gmail.com>
2023-10-25 14:46:32 +0900

    Auth0 login

--- src/main/java/com/iibc/spdataservice/LogoutHandler.java
+++ src/main/java/com/iibc/spdataservice/LogoutHandler.java
@@ -2,7 +2,6 @@ package com.iibc.spdataservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
@@ -16,44 +15,53 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Needed to perform SSO logout with Auth0. By default, Spring will clear the {@linkplain org.springframework.security.core.context.SecurityContext}
- * and the session. This controller will also log users out of Auth0 by calling the
- * <a href="https://auth0.com/docs/api/authentication?javascript&_ga=2.220855071.683371550.1592511441-1771410918.1592511441#logout">Auth0 logout endpoint</a>.
+ * Needed to perform SSO logout with Auth0. By default, Spring will clear the
+ * {@linkplain org.springframework.security.core.context.SecurityContext}
+ * and the session. This controller will also log users out of Auth0 by calling
+ * the
+ * <a href=
+ * "https://auth0.com/docs/api/authentication?javascript&_ga=2.220855071.683371550.1592511441-1771410918.1592511441#logout">Auth0
+ * logout endpoint</a>.
  */
 @Controller
-public class LogoutHandler extends SecurityContextLogoutHandler  {
-    
+public class LogoutHandler extends SecurityContextLogoutHandler {
+
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     /**
-     * Create a new instance with a {@code ClientRegistrationRepository}, so that we can look up information about the
-     * configured provider to call the Auth0 logout endpoint. Called by the Spring framework.
+     * Create a new instance with a {@code ClientRegistrationRepository}, so that we
+     * can look up information about the
+     * configured provider to call the Auth0 logout endpoint. Called by the Spring
+     * framework.
      *
-     * @param clientRegistrationRepository the {@code ClientRegistrationRepository} for this application.
+     * @param clientRegistrationRepository the {@code ClientRegistrationRepository}
+     *                                     for this application.
      */
-    @Autowired
     public LogoutHandler(ClientRegistrationRepository clientRegistrationRepository) {
         this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     /**
-     * Delegates to {@linkplain SecurityContextLogoutHandler} to log the user out of the application, and then logs
+     * Delegates to {@linkplain SecurityContextLogoutHandler} to log the user out of
+     * the application, and then logs
      * the user out of Auth0.
      *
-     * @param httpServletRequest the request.
+     * @param httpServletRequest  the request.
      * @param httpServletResponse the response.
-     * @param authentication the current authentication.
+     * @param authentication      the current authentication.
      */
     @Override
     public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-                       Authentication authentication) {
+            Authentication authentication) {
 
         // Invalidate the session and clear the security context
         super.logout(httpServletRequest, httpServletResponse, authentication);
 
-        // Build the URL to log the user out of Auth0 and redirect them to the home page.
-        // URL will look like https://YOUR-DOMAIN/v2/logout?clientId=YOUR-CLIENT-ID&returnTo=http://localhost:3000
+        // Build the URL to log the user out of Auth0 and redirect them to the home
+        // page.
+        // URL will look like
+        // https://YOUR-DOMAIN/v2/logout?clientId=YOUR-CLIENT-ID&returnTo=http://localhost:3000
         String issuer = (String) getClientRegistration().getProviderDetails().getConfigurationMetadata().get("issuer");
         String clientId = getClientRegistration().getClientId();
         String returnTo = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
@@ -73,7 +81,8 @@ public class LogoutHandler extends SecurityContextLogoutHandler  {
     }
 
     /**
-     * Gets the Spring ClientRegistration, which we use to get the registered client ID and issuer for building the
+     * Gets the Spring ClientRegistration, which we use to get the registered client
+     * ID and issuer for building the
      * {@code returnTo} query parameter when calling the Auth0 logout API.
      *
      * @return the {@code ClientRegistration} for this application.
--- src/main/java/com/iibc/spdataservice/SecurityConfig.java
+++ src/main/java/com/iibc/spdataservice/SecurityConfig.java
@@ -1,19 +1,25 @@
 package com.iibc.spdataservice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    
+
     private final LogoutHandler logoutHandler;
 
     public SecurityConfig(LogoutHandler logoutHandler) {
@@ -22,30 +28,48 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin(login -> login
-                .loginProcessingUrl("/login")
-                .loginPage("/login")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login?error")
-                .permitAll()
-        ).logout(logout -> logout
-                .logoutSuccessUrl("/")
-        ).authorizeHttpRequests(authz -> authz
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/images/**").permitAll()
-                .requestMatchers("/general").hasRole("GENERAL")
-                .requestMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
-        );
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login/oauth2")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userAuthoritiesMapper(grantedAuthoritiesMapper())));
         return http.build();
     }
 
-// If using HS256, create a Bean to specify the HS256 should be used. By default, RS256 will be used.
-//    @Bean
-//    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
-//        OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
-//        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> MacAlgorithm.HS256);
-//        return idTokenDecoderFactory;
-//    }
+    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach((authority) -> {
+                GrantedAuthority mappedAuthority;
+
+                if (authority instanceof OidcUserAuthority) {
+                    OidcUserAuthority userAuthority = (OidcUserAuthority) authority;
+                    mappedAuthority = new OidcUserAuthority(
+                            "OIDC_USER", userAuthority.getIdToken(), userAuthority.getUserInfo());
+                } else if (authority instanceof OAuth2UserAuthority) {
+                    OAuth2UserAuthority userAuthority = (OAuth2UserAuthority) authority;
+                    mappedAuthority = new OAuth2UserAuthority(
+                            "OAUTH2_USER", userAuthority.getAttributes());
+                } else {
+                    mappedAuthority = authority;
+                }
+
+                mappedAuthorities.add(mappedAuthority);
+            });
+
+            return mappedAuthorities;
+        };
+    }
+
+    // If using HS256, create a Bean to specify the HS256 should be used. By
+    // default, RS256 will be used.
+    // @Bean
+    // public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+    // OidcIdTokenDecoderFactory idTokenDecoderFactory = new
+    // OidcIdTokenDecoderFactory();
+    // idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration ->
+    // MacAlgorithm.HS256);
+    // return idTokenDecoderFactory;
+    // }
 }
